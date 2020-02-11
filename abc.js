d86f0ea16e1919f3cd10992d685c20590d308ea9
rahulpr123 <60912528+rahulpr123@users.noreply.github.com>
2020-02-11 15:50:37 +0530

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,1 +1,5 @@
-var person = [{Name:"Baluvo", class:"10", age:"22", display:function(){console.log(this.Name)}},{Name:"vichu", class:"10", age:"22", display:function(){console.log(this.Name)}}]
+var person = [{Name:"Baluvo", class:"10", age:"22", display:function(){console.log(this.Name)}},
+              {Name:"vichu", class:"10", age:"22", display:function(){console.log(this.Name)}}
+             {Name:"qads", class:"10", age:"21", display:function(){console.log(this.Name)}}
+             {Name:"don", class:"10", age:"22", display:function(){console.log(this.Name)}}
+             {Name:"fewu", class:"10", age:"22", display:function(){console.log(this.Name)}}]
